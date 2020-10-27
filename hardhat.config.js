ff114bc906877d5887332020f000fb08f768aa2a
Karl Floersch <karl@karlfloersch.com>
2020-10-27 19:37:55 -0400

    Add configurable mnemonic

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,5 +6,12 @@ require("@nomiclabs/hardhat-waffle");
 require("./tasks/faucet");
 
 module.exports = {
-  solidity: "0.7.3"
+  solidity: "0.7.3",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      }
+    }
+  }
 };
